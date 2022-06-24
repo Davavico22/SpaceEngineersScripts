88ebc5965777f21e59ae0bed9b868bbbf9a81dfa
David Valdivia Vico <david19valdivia98@gmail.com>
2022-06-24 15:46:20 +0200

    Agregado script de tracking solar

---
+++ Scripts/SolarTracking/VelocidadRotor.cs
@@ -0,0 +1,45 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+	partial class Program : MyGridProgram
+	{
+		//INICIO
+		IMyTextPanel lcd;
+		IMyMotorStator rotor;
+		public Program()
+		{
+			lcd = GridTerminalSystem.GetBlockWithName("p1") as IMyTextPanel;
+			rotor = GridTerminalSystem.GetBlockWithName("Rotor") as IMyMotorStator;
+		}
+
+		public void Main(string argument, UpdateType updateSource)
+		{
+			lcd.ContentType = ContentType.TEXT_AND_IMAGE;
+
+			float salida=rotor.TargetVelocityRPM;
+
+			lcd.WriteText("La velocidad es:" +salida);
+
+		}
+		//FIN
+	}
+}
